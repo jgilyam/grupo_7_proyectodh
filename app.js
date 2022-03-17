53c97dd18299679212e8da8a4d9f59fd44dd2cbf
InakiUnzaga <inakiunzaga88@gmail.com>
2022-03-17 18:31:04 -0300

    hacemos home

--- app.js
+++ app.js
@@ -17,10 +17,14 @@ app.get("/", (req, res) => {
 app.get("/register", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
+app.get("/home", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
 
 app.get("/login", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
+
 
 app.get("/product-cart", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
