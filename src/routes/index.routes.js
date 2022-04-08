6f5d6d9b62512ea6eebd10a702ae8688335313ed
Julieta Rosales <julietarosalesagostina@gmail.com>
2022-04-07 21:49:51 -0300

    cambio ruta principal por home

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const controllerPrincipal = require("../controllers/index.controllers");
 
-router.get("/", controllerPrincipal.index);
-router.get("/home",controllerPrincipal.home)
-router.get("/nosotros",controllerPrincipal.nosotros)
+/* router.get("/", controllerPrincipal.index); */
+router.get("/",controllerPrincipal.home);
+router.get("/home",controllerPrincipal.home);
+router.get("/nosotros",controllerPrincipal.nosotros);
 
 module.exports = router;
