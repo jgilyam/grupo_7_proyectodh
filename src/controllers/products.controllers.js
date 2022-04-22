219c56b27bfc730de4591ec45755b720845df061
jgilyam <jpgilyam@gmail.com>
2022-04-22 16:24:33 -0300

    se agrego ruta y  metodo en controlador para ver todos los productos, se creo vista products

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -1,6 +1,17 @@
+//modulos nativos
 const path = require("path");
+const fs = require("fs");
+
+//modulos de terceros
+
+//modulos propios
 const productRecomen = require("../models/baseProducts");
+
+//controloador a exportar
 const productControllers = {};
+
+//manejo de archivos para levantar base JSON
+const basePath = path.resolve(__dirname, "../data/products_DATA.json");
 
 productControllers.cart = (req, res) => {
   res.render("productCart");
@@ -13,7 +24,21 @@ productControllers.detail = (req, res) => {
 };
 
 productControllers.form = (req, res) => {
-  res.render('productForm');
-}
+  res.render("productForm");
+};
+//index, productos separados en 3 secciones, nuevo, ofertas, masVendido
+productControllers.index = (req, res) => {
+  //lectura de base de datos JSON
+  const products = JSON.parse(fs.readFileSync(basePath, "utf-8"));
+  //generación de 3 arrays separados en categorias.
+  let productsNew = products.filter((product) => product.category == "nuevo");
+  let productsInSale = products.filter(
+    (product) => product.category == "ofertas"
+  );
+  let productsVisit = products.filter(
+    (product) => product.category == "visited"
+  );
 
+  res.render("products", { productsNew, productsInSale, productsVisit });
+};
 module.exports = productControllers;
