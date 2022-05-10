039bb44deb121c92958e8fbe4a3e7fee5d47da28
Magali Garcia Muñoz <magalinicole.gm@gmail.com>
2022-05-10 10:12:13 -0300

    instalo express validator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const path = require("path");
 const express = require("express");
 const methodOverride = require("method-override");
 
-
 //modulos propios
 const routeMain = require("../src/routes/index.routes");
 const routeProducts = require("../src/routes/products.routes");
@@ -18,6 +17,7 @@ const app = express();
 const PORT = 4000; //puerto en el que el servidor escuchara
 
 //-----Configuración de express
+
 // se decalra la carpeta public para servir archivos estaticos
 app.use(express.static(path.resolve(__dirname, "../public")));
 //motor de plantillas ejs
@@ -25,13 +25,10 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(methodOverride("_method"));
 
-
-
 // Manejadores de rutas
 app.use("/", routeMain);
 app.use("/producto", routeProducts);
 app.use("/user", routeUsers);
-
 
 //Servidor
 app.listen(PORT, () => {
