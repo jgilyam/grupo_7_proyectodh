6bd9bed900213d59b904ef0edeef0c5633e8723d
JoseMariaUgarte <docdoyer99@gmail.com>
2022-05-09 22:07:04 -0300

    CREAR USUARIO-RUTA POST-MULTER USER- CONFIG DE EXPRESS- VIEW REGISTER

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,4 +1,10 @@
 const path = require("path");
+const fs = require('fs');
+
+//RUTA DE ARCHIVO JSON-USER
+const userFilePath = path.join(__dirname, "../data/users.json");
+//PASANDO ARCHIVO JSON-USER A ARRAY
+const users = JSON.parse(fs.readFileSync(userFilePath, "utf-8"));
 
 const controllersUser = {};
 
@@ -17,5 +23,33 @@ controllersUser.formconsultas = (req, res) => {
 controllersUser.preguntasFrecuentes = (req, res) => {
   res.render("pregFrecuentes");
 };
+controllersUser.createUser = (req, res)=>{
+    
+  let id = users[users.length - 1].id + 1;
+  let image = "default-image.png"
+
+  if (req.file) {
+    image = req.file.filename;
+  }
+
+  let createUser = {
+    id,
+    ...req.body,
+    image
+  };
+
+  const usersData = fs.readFileSync(userFilePath, 'utf-8');
+  let user;
+  if(usersData == ''){
+    user = [];
+  } else {
+    user = JSON.parse(usersData);
+  }
+  user.push(createUser);
+  usersJSON = JSON.stringify(user);
+  fs.writeFileSync(userFilePath, usersJSON);
+  
+  res.redirect('../home')
+}
 
 module.exports = controllersUser;
