dd50649bf559b493ea6b20fa311209544fc1d6c5
Magali Garcia Muñoz <magalinicole.gm@gmail.com>
2022-05-18 16:42:52 -0300

    agrego validaciones y middleware a ruta de proccesRegister

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -26,8 +26,29 @@ const validations = [
   body("first_name").notEmpty().withMessage("Debe escribir el nombre"),
   body("last_name").notEmpty().withMessage("Debe escribir el apellido"),
   body("image").notEmpty().withMessage("Debe subir una imagen"),
-  body("email").notEmpty().withMessage("Debe escribir su email"),
-  body("contraseña").notEmpty().withMessage("Debe escirbir su contrasena"),
+  body("email")
+    .notEmpty()
+    .withMessage("Debe escribir su email")
+    .bail()
+    .isEmail()
+    .withMessage("Debes escribir un formato de correo electronico valido"),
+  body("contraseña").notEmpty().withMessage("Debe escirbir su contraseña"),
+  body("image").custom((value, { req }) => {
+    let file = req.file;
+    //*let acceptedExtensions = [".jpg", ".png", ".gif"]; //fijarse esto!!
+
+    if (!file) {
+      throw new Error("Debes subir una imagen");
+    } /*else {
+      let fileExtensions = path.extname(file.originalname);
+      if (!acceptedExtensions.includes(fileExtensions)) {
+        throw new Error(
+          `Las extensiones permitidas son ${acceptedExtensions.join(",")}`
+        );
+      }
+    }*/
+    return true;
+  }),
 ];
 
 router.get("/register", controllersUser.register);
@@ -36,7 +57,7 @@ router.post(
   "/register",
   upload.single("image"),
   validations,
-  controllersUser.createUser
+  controllersUser.processRegister
 );
 
 router.get("/login", controllersUser.login);
