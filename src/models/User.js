b17f9bde747350c1a2e575c86f857c3de2680396
Magali Garcia Muñoz <magalinicole.gm@gmail.com>
2022-05-18 16:44:08 -0300

    cambio de lugar el createUser para acomodar

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const path = require("path");
+
+const userFilePath = path.join(__dirname, "../data/users.json");
+const users = JSON.parse(fs.readFileSync(userFilePath, "utf-8"));
 
 const User = {
   fileName: "./data/users.json",
@@ -9,4 +13,35 @@ const User = {
   findAll: function () {
     return this.getData;
   },
+
+  createUser: (req, res) => {
+    let id = users[users.length - 1].id + 1;
+
+    let image = "default-image.png";
+
+    if (req.file) {
+      image = req.file.filename;
+    }
+
+    let createUser = {
+      id,
+      ...req.body,
+      image,
+    };
+
+    const usersData = fs.readFileSync(userFilePath, "utf-8");
+    let user;
+    if (usersData == "") {
+      user = [];
+    } else {
+      user = JSON.parse(usersData);
+    }
+    user.push(createUser);
+    usersJSON = JSON.stringify(user);
+    fs.writeFileSync(userFilePath, usersJSON);
+
+    res.redirect("../home");
+  },
 };
+
+module.exports = User;
