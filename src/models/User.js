4437c83301be98ec4235fdd57aed170ef5bc2de8
SIAF AC Rosales Julieta Agostina <jarosales@ara.mil.ar>
2022-05-20 10:39:32 -0300

    agrego en user logica para procesos dentro del form y login

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,64 @@
 const fs = require("fs");
 const path = require("path");
 
-const userFilePath = path.join(__dirname, "../data/users.json");
-const users = JSON.parse(fs.readFileSync(userFilePath, "utf-8"));
+const database = path.join(__dirname, "../data/users.json");
+ 
+
+const bcryptjs = require("bcryptjs")
+
+const user = {
+  fileName: database,
+
+  getData : function(){
+    return JSON.parse(fs.readFileSync(this.fileName, "utf-8"));
+  },
+
+  /* findByPK:function (id){
+    para obtener todos los usuarios por id
+  } */
+
+  /* findByEmail: function(email){
+    para obtener todos los usuarios por email
+  } */
+  
+  generarId: function(){ //genero id buscando el ultimo y si no encuentrar traer id 1
+    let allUsers = this.getData();
+    let ultimoUser = allUsers.pop()
+    if (ultimoUser){
+      return ultimoUser.id + 1
+    }
+    return 1;
+  },
+
+  create : function(userData,image){
+    let allUsers = this.getData()//me traigo todos los usuarios
+
+    //guardo imagen por defecto
+    let imageFinal = "default-image.png"
+  
+    if(image){
+      imageFinal = image.filename
+    }
+
+    let newUser = {
+      id: this.generarId(),
+      ...userData,
+      /* constraseña : bcryptjs.hashSync(userData.contraseña,10), */
+      imageUsuario : imageFinal
+    };
+
+    allUsers.push(newUser)// luego de esto vamos a escrbirlo en el archivo JSON
+    fs.writeFileSync(this.fileName,JSON.stringify(allUsers,null, ""))//el write te pide ¿donde lo vas a escribir? en este caso en nuestro valor fileName mas arriba que tiene nuestro JSON//despues le pasamos en formato JSOn con stringify//si quiero mantener el formato edl JSON original pasar dos parametros mas null y un espacio;
+    return newUser
+  }
+
+}
+
+module.exports = user
+
+
+
+
 
 /*const User = {
   fileName: "./data/users.json",
