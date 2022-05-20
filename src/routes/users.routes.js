defa168cd9eb7d9e003125ce0e63e310fde1770a
SIAF AC Rosales Julieta Agostina <jarosales@ara.mil.ar>
2022-05-20 10:34:44 -0300

    agrego ruta de validationRegister y paso el array de validations a middelware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controllersUser = require("../controllers/user.controllers");
 const multer = require("multer");
 const path = require("path");
-
+const validations = require("../middleware/validationRegister")
 //requiero express-validator
 const { body } = require("express-validator");
 
@@ -21,35 +21,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-//* VALIDACIONES
-const validations = [
-  body("first_name").notEmpty().withMessage("Debe escribir el nombre"),
-  body("last_name").notEmpty().withMessage("Debe escribir el apellido"),
-  //body("image").notEmpty().withMessage("Debe subir una imagen"),
-  body("email")
-    .notEmpty()
-    .withMessage("Debe escribir su email")
-    .bail()
-    .isEmail()
-    .withMessage("Debes escribir un formato de correo electronico valido"),
-  body("contraseña").notEmpty().withMessage("Debe escirbir su contraseña"),
-  /*body("image").custom((value, { req }) => {
-    let file = req.file;
-    let acceptedExtensions = [".jpg", ".png", ".gif"];
 
-    if (!file) {
-      throw new Error("Debes subir una imagen");
-    } else {
-      let fileExtensions = path.extname(file.originalname);
-      if (!acceptedExtensions.includes(fileExtensions)) {
-        throw new Error(
-          `Las extensiones permitidas son ${acceptedExtensions.join(",")}`
-        );
-      }
-    }
-    return true;
-  }),*/
-];
 
 router.get("/register", controllersUser.register);
 
