61d7f66c76bf331e1dae0b215ba0786fc3b16883
Magali Garcia Muñoz <magalinicole.gm@gmail.com>
2022-05-21 13:34:00 -0300

    agrego funciones de buscar por id y por email

--- src/models/user.js
+++ src/models/user.js
@@ -2,66 +2,65 @@ const fs = require("fs");
 const path = require("path");
 
 const database = path.join(__dirname, "../data/users.json");
- 
 
-const bcryptjs = require("bcryptjs")
+const bcryptjs = require("bcryptjs");
 
 const user = {
   fileName: database,
 
-  getData : function(){
+  getData: function () {
     return JSON.parse(fs.readFileSync(this.fileName, "utf-8"));
   },
 
-  /* findByPK:function (id){
-    para obtener todos los usuarios por id
-  } */
-
-  /* findByEmail: function(email){
-    para obtener todos los usuarios por email
-  } */
-  
-  generarId: function(){ //genero id buscando el ultimo y si no encuentrar traer id 1
+  findByPK: function (id) {
+    //obtener a todos los usuario por id
     let allUsers = this.getData();
-    let ultimoUser = allUsers.pop()
-    if (ultimoUser){
-      return ultimoUser.id + 1
+    let userFound = allUsers.find((oneUser) => oneUser.id === id);
+    return userFound;
+  },
+
+  findByEmail: function (email) {
+    //obtener a todos los usuario por email
+    let allUsers = this.getData();
+    let userFoundEmail = allUsers.find((oneUser) => oneUser.email === email);
+    return userFoundEmail;
+  },
+
+  generarId: function () {
+    //genero id buscando el ultimo y si no encuentrar traer id 1
+    let allUsers = this.getData();
+    let ultimoUser = allUsers.pop();
+    if (ultimoUser) {
+      return ultimoUser.id + 1;
     }
     return 1;
   },
 
-  create : function(userData,image){
-    let allUsers = this.getData()//me traigo todos los usuarios
+  create: function (userData, image) {
+    let allUsers = this.getData(); //me traigo todos los usuarios
 
     //guardo imagen por defecto
-    let imageFinal = "default-image.png"
-  
-    if(image){
-      imageFinal = image.filename
-    }
+    let imageFinal = "default-image.png";
 
+    if (image) {
+      imageFinal = image.filename;
+    }
 
     let newUser = {
       id: this.generarId(),
       ...userData,
-      contraseña : bcryptjs.hashSync(userData.contraseña,10),
-      passwordRepit : bcryptjs.hashSync(userData.passwordRepit,10),
-      imageUsuario : imageFinal
+      contraseña: bcryptjs.hashSync(userData.contraseña, 10),
+      passwordRepit: bcryptjs.hashSync(userData.passwordRepit, 10),
+      imageUsuario: imageFinal,
     };
 
+    allUsers.push(newUser); // luego de esto vamos a escrbirlo en el archivo JSON
+    fs.writeFileSync(this.fileName, JSON.stringify(allUsers, null, "")); //el write te pide ¿donde lo vas a escribir? en este caso en nuestro valor fileName mas arriba que tiene nuestro JSON//despues le pasamos en formato JSOn con stringify//si quiero mantener el formato edl JSON original pasar dos parametros mas null y un espacio;
+    return newUser;
+  },
+};
 
-    allUsers.push(newUser)// luego de esto vamos a escrbirlo en el archivo JSON
-    fs.writeFileSync(this.fileName,JSON.stringify(allUsers,null, ""))//el write te pide ¿donde lo vas a escribir? en este caso en nuestro valor fileName mas arriba que tiene nuestro JSON//despues le pasamos en formato JSOn con stringify//si quiero mantener el formato edl JSON original pasar dos parametros mas null y un espacio;
-    return newUser
-  }
-
-}
-
-module.exports = user
-
-
-
-
+module.exports = user;
 
 /*const User = {
   fileName: "./data/users.json",
