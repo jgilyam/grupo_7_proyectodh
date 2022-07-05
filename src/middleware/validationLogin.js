9d45a049e0103e7be938ca08d03288f50c41ae6d
jgilyam <jgilyam@folcode.com>
2022-07-05 18:27:37 -0300

    se generan reglas de validacion para login

--- src/middleware/validationLogin.js
+++ src/middleware/validationLogin.js
@@ -1,12 +1,14 @@
-
-
 //vamos a requerir express-validator
 
 const { check } = require("express-validator");
 
 const validacionesLogin = [
-  check("email").isEmail().withMessage("Email invalido"),
-  check("password").notEmpty().withMessage("Contraseña invalida"),
+  check("email")
+    .notEmpty()
+    .withMessage("Email requerido")
+    .isEmail()
+    .withMessage("Email invalido"),
+  check("password").notEmpty().withMessage("Contraseña requerida"),
 ];
 
 module.exports = validacionesLogin;
