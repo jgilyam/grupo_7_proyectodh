ad536b4ae9fecd6604796c55bb576eb8480527a3
Magali Garcia Muñoz <magalinicole.gm@gmail.com>
2022-07-14 14:35:56 -0300

    api user

--- src/controllers/api/user.controllers.js
+++ src/controllers/api/user.controllers.js
@@ -2,9 +2,26 @@
 const db = require("../../../database/models");
 
 const userController = {
-  /* list: (req, res) => {
-   juampiiiiiiii
-  }, */
+  list: (req, res) => {
+    db.User.findAll().then((users) => {
+      let response = {
+        status: 200,
+        count: users.length,
+        users: [],
+      };
+
+      users.forEach((user) => {
+        response.users.push({
+          id: user.id_user,
+          name: user.firs_name,
+          email: user.email,
+          detail: `${req.originalUrl}/${user.id_user}`,
+        });
+      });
+
+      return res.status(200).json([response]);
+    });
+  },
   detail: (req, res) => {
     db.User.findByPk(req.params.id).then((user) => {
       res.json({
--- src/routes/api/users.router.js
+++ src/routes/api/users.router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../../controllers/api/user.controllers");
 
-/* router.get("/", userController.list); */
+router.get("/", userController.list);
 router.get("/:id", userController.detail);
 
 module.exports = router;
