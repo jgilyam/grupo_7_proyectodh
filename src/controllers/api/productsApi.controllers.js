dca0a73ce8823f836e3bf9269f0fae900cf9b21e
JoseMariaUgarte <docdoyer99@gmail.com>
2022-07-24 22:06:10 -0300

    camio algunas cosas de api detail

--- src/controllers/api/productsApi.controllers.js
+++ src/controllers/api/productsApi.controllers.js
@@ -104,7 +104,8 @@ const productsApiController = {
             description: productos.description,
             stock:productos.stock,
             discount:productos.discount,
-            relation:{Category:[{id :productos.id_category, name: categoriaProducto}],Type:[{id:productos.id_type, name:typeProducto}]},
+            category:categoriaProducto,
+            type:typeProducto,
             imagen: `http://localhost:4000/api/products/imagen/${productos.product_image}`
           },
         },
