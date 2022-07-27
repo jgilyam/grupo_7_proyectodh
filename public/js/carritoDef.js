55557dbfe646c784a7191af77ca4d0a0967974f2
jgilyam <jgilyam@folcode.com>
2022-07-27 08:03:11 -0300

    trayendo info de local storage y pintando items

--- public/js/carritoDef.js
+++ public/js/carritoDef.js
@@ -1,11 +1,24 @@
-const cajaCarrito = document.querySelector(".container-caja");
+window.addEventListener("load", () => {
+  let carrito = document.querySelector(".container-cards-carrito");
 
+  let item = document.querySelector(".conteiner-caja");
+  let HTMLitem = item.innerHTML;
+  item.remove();
+  const productosEnCarrito = JSON.parse(
+    localStorage.getItem("Productos Guardados")
+  );
+  console.log(productosEnCarrito);
 
-function addProduct() {
-  let products = [];
-  if (localStorage.getItem("Productos Guardados")) {
-    products = JSON.parse(localStorage.getItem("Productos Guardados"));
-  }
-  products.push({ productId: productId + 1, image: "<imageLink>" });
-  localStorage.setItem("Productos Guardados", JSON.stringify(products));
-}
+  productosEnCarrito.forEach((producto, i) => {
+    let newItem = document.createElement("div");
+    newItem.className = "conteiner-caja";
+    newItem.id = `card-n${i}`;
+    newItem.innerHTML = HTMLitem;
+    carrito.appendChild(newItem);
+    let itemToEdit = document.querySelector(`#card-n${i}`);
+    let title = itemToEdit.querySelector(".parrafo1");
+    let img = itemToEdit.querySelector(`#card-n${i} img`);
+    img.setAttribute("src", producto.img);
+    title.innerText = producto.title;
+  });
+});
